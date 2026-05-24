784173c046d8b8ab6ca1fa26af08ecd5744a2b4a
Snehadey-2202 <dey22022005@gmail.com>
2026-05-24 10:31:51 +0530

    Fix model architecture with BatchNorm and GAP

--- ml/model.py
+++ ml/model.py
@@ -6,29 +6,38 @@ class PotholeCNN(nn.Module):
     def __init__(self, num_classes=2):
         super(PotholeCNN, self).__init__()
         
-        # Convolutional layers
+        # Convolutional layers with Batch Normalization to stabilize training
         self.conv1 = nn.Conv2d(in_channels=3, out_channels=32, kernel_size=3, padding=1)
+        self.bn1 = nn.BatchNorm2d(32)
+        
         self.conv2 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3, padding=1)
+        self.bn2 = nn.BatchNorm2d(64)
+        
         self.conv3 = nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3, padding=1)
+        self.bn3 = nn.BatchNorm2d(128)
         
         # Max pooling layer
         self.pool = nn.MaxPool2d(kernel_size=2, stride=2)
         
+        # Global Average Pooling to reduce dimensions to 1x1 per channel
+        # This prevents the massive parameter explosion of flattening the whole image
+        self.gap = nn.AdaptiveAvgPool2d((1, 1))
+        
         # Fully connected layers
-        # Input image size: 224x224
-        # After 3 pooling layers: 224 -> 112 -> 56 -> 28
-        # Flattened size: 128 channels * 28 * 28
-        self.fc1 = nn.Linear(128 * 28 * 28, 512)
+        self.fc1 = nn.Linear(128, 64)
         self.dropout = nn.Dropout(0.5)
-        self.fc2 = nn.Linear(512, num_classes)
+        self.fc2 = nn.Linear(64, num_classes)
 
     def forward(self, x):
         # Layer 1
-        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.bn1(self.conv1(x))))
         # Layer 2
-        x = self.pool(F.relu(self.conv2(x)))
+        x = self.pool(F.relu(self.bn2(self.conv2(x))))
         # Layer 3
-        x = self.pool(F.relu(self.conv3(x)))
+        x = self.pool(F.relu(self.bn3(self.conv3(x))))
+        
+        # Global Average Pooling
+        x = self.gap(x)
         
         # Flatten
         x = x.view(x.size(0), -1)
@@ -39,3 +48,4 @@ class PotholeCNN(nn.Module):
         x = self.fc2(x)
         
         return x
+
